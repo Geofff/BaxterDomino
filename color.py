e8ceb72f371547d39d93a23ab43090512f33283e
Alastairsc <gassyhue@hotmail.com>
2017-10-28 16:07:06 +0800

    Recognised cross as origin and can find position of dominoes relative to that

--- color.py
+++ color.py
@@ -26,48 +26,89 @@ class color_detector:
 
     def filterColors(self, image):
         """
-            Finds each face of all dominoes in image and gets the position and
-            orientation
+            Finds each face of all dominoes in image
         :param image to analyse
-        :return list of tuples (faceImage, xPos, yPos, angle) ordered by distance
+        :return list of images of each domino face
         """
         results = []
-        size = image.shape
-        print "Size: ", size[0], " ", size[1]
-        xPoint = 175
-        yPoint = 345
+        #size = image.shape
+        #print "Size: ", size[0], " ", size[1]
+        #xPoint = 200
+        #yPoint = 350
         #hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         #for i in range(-5,5):
-            #for j in range(-5,5):
-                #print hsv_image.item((xPoint + i,yPoint + j,0)),
-                #print hsv_image.item((xPoint + i, yPoint + j, 1)),
-                #print hsv_image.item((xPoint + i, yPoint + j, 2))
-                #image[xPoint + i, yPoint + j, :] = [255, 255, 255]
+        #    for j in range(-5,5):
+        #        print image.item((xPoint + i,yPoint + j,0)),
+        #        print image.item((xPoint + i, yPoint + j, 1)),
+        #        print image.item((xPoint + i, yPoint + j, 2))
+        #        image[xPoint + i, yPoint + j, :] = [255, 255, 255]
         #cv2.imshow("images", image)
         #cv2.waitKey(0)
-        #for i in range(0,3):
-         #   print(i," ", image.item((xPoint, yPoint,i)))
         colors = self.__getColors(image)
         sides = []
-        #cv2.imshow("images", image)
-        #cv2.waitKey(0)
         print "Displaying Colours: (", len(colors), ")"
         for color in colors:
             cv2.imshow("images", color)
             cv2.waitKey(0)
             sides.extend(self.__getConnectedComponents(color))
+        return sides
+
+    def getAllDominoes(self, image):
+        """
+            Finds all dominoes in the image and finds their position and
+            orientation ordered by face size
+        :param image from camera
+        :return list of tuples (xPos, yPos, orientation, locationImage)
+        """
+        faces = self.filterColors(image)
         faceQueue = []
-        for side in sides:
-            xPos, yPos = self.__getCentres(side)
-            phi = self.__getOrientation(side)
-            location = self.drawLocation(side, (xPos, yPos), phi)
-            faceSize = self.__getFaceSize(side)
-            print(faceSize)
-            heapq.heappush(faceQueue, (-faceSize, (location,xPos,yPos,phi)))
+        results = []
+        for face in faces:
+            xPos, yPos = self.__getCentres(face)
+            phi = self.__getOrientation(face)
+            location = self.drawLocation(face, (xPos, yPos), phi)
+            faceSize = self.__getFaceSize(face)
+            heapq.heappush(faceQueue, (-faceSize, (xPos, yPos, phi, location)))
         for i in range(len(faceQueue)):
             results.append(heapq.heappop(faceQueue)[1])
         print("Faces found: ", len(results))
         return results
+
+
+    def getNextDomino(self, image):
+        """
+            Gets domino with largest face (closest) from image. Returns empty
+            tuple if no faces are found
+        :param image from camera
+        :return tuple (xPos, yPos, orientation, locationImage)
+        """
+        faces = self.filterColors(image)
+        max = 0
+        maxDomino = ()
+        for i, face in enumerate(faces):
+            faceSize = self.__getFaceSize(face)
+            if faceSize > max:
+                max = faceSize
+                maxDomino = i
+        xPos, yPos = self.__getCentres(faces[maxDomino])
+        phi = self.__getOrientation(faces[maxDomino])
+        locationImage = self.drawLocation(faces[maxDomino], (xPos, yPos), phi)
+        return (xPos, yPos, phi, locationImage)
+
+    def toNextDomino(self, image):
+        """
+            Gets position of next domino relative to origin. Returns empty
+            tuple if no faces are found
+        :param image from camera
+        :return tuple (relativeDistance, relativeAngle, locationImage)
+        """
+        nextDomino = self.getNextDomino(image)
+        if nextDomino:
+            relDist, relAngle, origin = self.getPosRelative(image, nextDomino)
+            outImage = self.drawPoint(image.copy(), (origin[0], origin[1]))
+            self.drawLocation(outImage, (nextDomino[0], nextDomino[1]), origin[2] + relAngle)
+            return (relDist, relAngle, outImage)
+        return nextDomino
 
     def getLongEdgeLength(self, faceTuple):
         """
@@ -89,6 +130,231 @@ class color_detector:
             i += 1
         dist = math.sqrt(pow(max[0]-faceTuple[1],2) + pow(max[0]-faceTuple[2],2))
         return dist * 2, output
+
+    def getOrigin(self, image):
+        """
+            Finds position and orientation of origin represented by
+            black cross from image.
+        :param image from camera
+        :return tuple (xPos, yPos, orientation)
+        """
+        lowerBound = np.array([215, 210, 215], dtype="uint8")
+        upperBound = np.array([255, 255, 255], dtype="uint8")
+        invImage = cv2.bitwise_not(image.copy())
+        mask = cv2.inRange(invImage, lowerBound, upperBound)
+        output = cv2.bitwise_and(invImage, invImage, mask=mask)
+        ouput = cv2.medianBlur(output, 9)
+        output = self.__removeManipulator(output)
+        print "Manipulator removed"
+        cv2.imshow("images", np.hstack([image, output]))
+        cv2.waitKey(0)
+        skel = self.__blur(self.__getSkeleton(output), 3)
+        xtremes = self.__getExtremePoints(skel)
+        xtremeImage = image.copy()
+        for p in xtremes:
+            xtremeImage = self.drawPoint(xtremeImage, p)
+        xPos, yPos = self.__getIntersection(xtremes)
+        orientation = self.__getOrientationPoints(xPos, yPos, xtremes)
+        return (xPos, yPos, orientation)
+
+    def drawLocation(self, image, center, angle):
+        """
+            Draw location of dominoe on image given position and orientation
+        :param image to draw on:
+        :param center position of domino (x,y):
+        :param angle of domino in degrees:
+        :return image with position drawn in white:
+        """
+        size = image.shape
+        image = self.drawPoint(image, (center[1], center[0]))
+        x = center[0] + 15 * math.sin(angle * math.pi / 180 + 90)
+        y = center[1] + 15 * math.cos(angle * math.pi / 180 + 90)
+        cv2.line(image, (center[0], center[1]), (int(x), int(y)), (255,255,255))
+        return image
+
+    def drawPoint(self, image, point):
+        """
+            Draws given point on image using a white cross
+        :param image to draw on
+        :param point to draw
+        :return same image with point drawn on
+        """
+        point = (int(point[0]), int(point[1]))
+        for i in range(-5, 6):
+            image[point[0] + i, point[1], :] = [255,255,255]
+            image[point[0], point[1] + i, :] = [255,255,255]
+        return image
+
+    def getPosRelative(self, image, position):
+        """
+            Gets relative position of given domino position and orientation
+            with origin found in image
+        :param image from camera
+        :param position of domino (xPos, yPos, orientation)
+        :return dist - relative distance to origin
+        :return angle - relative from origin angle
+        :return originPos - tuple (xPos, yPos, orientation)
+        """
+        originPos = self.getOrigin(image)
+        dx = originPos[0] - position[0]
+        dy = originPos[1] - position[1]
+        dist = math.sqrt(pow(dx,2) + pow(dy,2))
+        angle = math.atan2(dy, dx) * 180 / math.pi
+        return dist, angle - originPos[2], originPos
+
+    def __getOrientationPoints(self, xPos, yPos, points):
+        """
+            Gets orientation of origin given extreme points
+        :param xPos of centre of mass
+        :param yPos of centre of mass
+        :param points extremities of origin
+        :return angle of origin in degrees
+        """
+        max = 0
+        maxPoint = 0
+        for i, (x, y) in enumerate(points):
+            dist = math.sqrt(pow(xPos-x, 2) + pow(yPos-y, 2))
+            if dist > max:
+                max = dist
+                maxPoint = i
+        return math.atan2(points[maxPoint][1] - yPos,points[maxPoint][0] - xPos) * 180 / math.pi
+
+    def __getIntersection(self, points):
+        """
+            Finds point where cross intersects given by line endpoints.
+            Ie center of origin
+        :param points list of 4 points (x,y) of line endpoints
+        :return: x, y coordinates of intersection point
+        """
+        points = self.__reversePoints(points)
+        #cv2.line(image, points[0], points[1], (255,255,255))
+        # cv2.line(image, points[2], points[3], (255, 255, 255))
+        x1 = points[0][0]
+        y1 = points[0][1]
+        m1 = float(points[1][1] - points[0][1])/float(points[1][0] - points[0][0])
+        x2 = points[2][0]
+        y2 = points[2][1]
+        m2 = float(points[3][1] - points[2][1])/float(points[3][0] - points[2][0])
+        xPos = (y2 - y1 + m1 * x1 - m2 * x2)/(m1-m2)
+        yPos = m1 * (xPos -x1) + y1
+        return int(xPos), int(yPos)
+
+    def __reversePoints(self, points):
+        """
+            Reverses orderering of a list of tuples
+        :param points list of tuples
+        :return list of reversed tuples
+        """
+        output = []
+        for p in points:
+            output.append(((p[1]), p[0]))
+        return output
+
+    def __getExtremePoints(self, image):
+        """
+            Gets extremeties of given image of origin (skeletonised)
+        :param BGR image to analyse
+        :return list of 4 endpoints of lines
+        """
+        imOut = image.copy()
+        result = []
+        size = image.shape
+        first = ()
+        last = ()
+        for i in range(size[0]):
+            for j in range(size[1]):
+                if image[i,j,0] > 0:
+                    if not first: first = (i,j)
+                    else: last = (i, j)
+        result.append(first)
+        result.append(last)
+        first = ()
+        for j in range(size[1]):
+            for i in range(size[0]):
+                if image[i,j,0] > 0:
+                    if not first: first =  (i,j)
+                    else: last = (i,j)
+        result.append(first)
+        result.append(last)
+        return result
+
+    def __blur(self, image, num):
+        """
+            Removes points with less than num neighbours
+        :param image to analyse
+        :param num min number of neighbours
+        :returnm image with points removed
+        """
+        output = image.copy()
+        size = image.shape
+        for i in range(size[0]):
+            for j in range(size[1]):
+                if image[i,j,0] > 0:
+                    neighbours  = 0
+                    for m in range(i-1,i+2):
+                        for n in range(j-1, j+2):
+                            if (not (m == i and n == j)) and image[m,n,0] > 0:
+                                neighbours += 1
+                    if neighbours < num:
+                        output[i,j,:] = [0,0,0]
+        return output
+
+    def __getSkeleton(self, image):
+        """
+            Performs skeletonisation of given BGR image
+            Source: http://opencvpython.blogspot.com.au/2012/05/skeletonization-using-opencv-python.html
+        :param image to analyse
+        :return BGR skeleton of image
+        """
+        image = cv2.medianBlur(image, 9)
+        gray = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
+        ret, gray = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+        size = np.size(gray)
+        #cv2.imshow("images", gray)
+        skel = np.zeros(gray.shape, dtype="uint8")
+        #cv2.waitKey(0)
+        element =  cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
+        done = False
+        while not done:
+            eroded = cv2.erode(gray, element)
+            temp = cv2.dilate(eroded, element)
+            temp = cv2.subtract(gray, temp)
+            skel = cv2.bitwise_or(skel, temp)
+            gray = eroded.copy()
+
+            zeros = size - cv2.countNonZero(gray)
+            if zeros == size:
+                done = True
+        output = np.zeros(image.shape, dtype="uint8")
+        size = output.shape
+        for i in range(size[0]):
+            for j in range(size[1]):
+                if skel[i,j] > 0:
+                    output[i,j,:] = [255,255,255]
+        return output
+
+
+    def __removeManipulator(self, image):
+        """
+            Removes manipulator from image. NOT TESTED
+        :param image to edit
+        :return image with manipulators whited out
+        """
+        size = image.shape
+        edge = []
+        empty = True
+        for i in range(size[0]):
+            if image[i,0,0] > 0 and empty:
+                edge.append(i)
+                empty = False
+            elif image[i,0,0] < 1 and not empty:
+                edge.append(i)
+                empty = True
+        man1 = int((edge[0] + edge[1])/2)
+        man2 = int((edge[2] + edge[3])/2)
+        image = cv2.rectangle(image, (man1-50,100), (man1+50,200), (0,0,0))
+        image = cv2.rectangle(image, (man2-50,100), (man2+50,200), (0,0,0))
+        return image
 
     def __getFaceSize(self, face):
         """
@@ -112,29 +378,7 @@ class color_detector:
             factor = 2.4
         return np.sum(face) * factor
 
-    def drawLocation(self, image, center, angle):
-        """
-            Draw location of dominoe on image given position and orientation
-        :param image to draw on:
-        :param center position of domino (x,y):
-        :param angle of domino in degrees:
-        :return image with position drawn in white:
-        """
-        size = image.shape
-        for i in range(-5, 6):
-            image[center[0] + i, center[1], :] = [255,255,255]
-            image[center[0], center[1] + i, :] = [255,255,255]
-        grad = math.tan(angle * math.pi / 180)
-        for i in range(15):
-            y = center[1] + i * grad
-            if y >= size[1]:
-                if center[1] + i < size[1]:
-                    image[center[0], center[1] + i, :] = [255, 255, 255]
-            else:
-                if center[0] + i >= size[0]:
-                    image[size[0] - 1, int(y), :] = [255,255,255]
-                image[center[0] + i, int(y), :] = [255,255,255]
-        return image
+
 
     def __getColors(self, image):
         """
@@ -213,12 +457,24 @@ class color_detector:
         return math.atan(grad) * 180 / math.pi
 
     def __isPerpendicular(self, grad1, grad2):
+        """
+            Checks to see whether two gradients are perpendicular
+        :param grad1:
+        :param grad2:
+        :return:
+        """
         angle1 = math.atan(grad1) * 180 / math.pi
         angle2 = math.atan(grad2) * 180 / math.pi
         diff = int(angle1 - angle2) % 90
         return abs(diff) < 2
 
     def __getRange(self, x, y):
+        """
+            Gets range of x & y coordinates
+        :param x:
+        :param y:
+        :return:
+        """
         return math.sqrt(pow(x[-1] - x[0], 2) + pow(y[-1] - y[0], 2))
 
     def __getGradient(self, x, y):
@@ -365,13 +621,19 @@ def main():
         return 0;
     cd = color_detector()
     img = cv2.imread(sys.argv[1], 1)
-    faces = cd.filterColors(img)
-    for face in faces:
-        cv2.imshow("images", np.hstack([img, face[0]]))
+
+    img = cv2.resize(img, (640, 400), interpolation=cv2.INTER_CUBIC)
+    #faces = cd.filterColors(img)
+    xPos, yPos, phi = cd.getOrigin(img)
+    originImage = cd.drawLocation(img.copy(),(xPos, yPos), phi)
+    cv2.imshow("images", np.hstack([img, originImage]))
+    cv2.waitKey(0)
+    #for face in faces:
+        #cv2.imshow("images", np.hstack([img, face[0]]))
     #    cv2.waitKey(0)
-    edgeLength, lengthImage = cd.getLongEdgeLength(faces[0])
-    print("Length: ", edgeLength)
-    cv2.imshow("images", np.hstack([img, lengthImage]))
+    #edgeLength, lengthImage = cd.getLongEdgeLength(faces[0])
+    #print("Length: ", edgeLength)
+    #cv2.imshow("images", np.hstack([img, lengthImage]))
    # cv2.waitKey(0)
     return 0
 
--- detector.py
+++ detector.py
@@ -18,12 +18,14 @@ from matplotlib import pyplot as plt
 
 
 class image_converter:
-    def __init__(self, image_topic):
+    def __init__(self):
+        image_topic = "/cameras/left_hand_camera/image"
         self.image_pub = rospy.Publisher("/robot/xdisplay", Image, latch=True, queue_size=10)
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber(image_topic, Image, self.callback)
         self.cd = color.color_detector()
         self.ed = edge.edge_detector()
+        self.position = ()
 
     def display_image(self, msg):
         print('Publishing to display')
@@ -39,35 +41,30 @@ class image_converter:
         print("Obtained image..")
         edges = self.ed.getEdgesF(cv_image, 1000, 5000)
         edgeImage = self.ed.mergeEdges(cv_image, edges)
-        print("Display Edges...")
+        #print("Display Edges...")
         #cv2.imshow("images", np.hstack([cv_image, edgeImage]))
         #cv2.waitKey(0)
-        faces = self.cd.filterColors(cv_image)
-        print("Located Dominoes: ", len(faces))
-        for face in faces:
-            print("Location: ", face[1], ", ", face[2], " Orientation: ", face[3])
-            #edgeLength, lengthImage = self.cd.getLongEdgeLength(face)
-            #print("Length: ", edgeLength)
-        #cv2.waitKey(0)
-        if faces:
-            msg = self.cd.drawLocation(cv_image, (faces[0][1], faces[0][2]), faces[0][3])
+        nextDomino = self.cd.toNextDomino(edgeImage)
+        if nextDomino:
+            msg = nextDomino[2]
+            self.position = (nextDomino[0], nextDomino[1])
         else:
-            "No faces found"
+            print "No faces found"
             msg = cv_image
         try:
             cv2.imshow("images", msg)
             msg = self.bridge.cv2_to_imgmsg(cv2.resize(msg,(1024, 600), interpolation = cv2.INTER_CUBIC), "bgr8")
             self.display_image(msg)
-            #cv2.waitKey(0)
         except CvBridgeError as e:
             print(e)
 
+    def getNextDomino(self):
+        return self.position
 
 def main(args):
     #setup_cameras()
     #rospy.init_node('image_listener')
-    image_topic = "/cameras/left_hand_camera/image"
-    ic = image_converter(image_topic)
+    ic = image_converter()
     print("Initialising...")
     rospy.init_node('image_converter', anonymous=True)
     try:
