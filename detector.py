dd341bc4d021a2abad6dac7d6736d0dbbcc6b54a
Alastairsc <gassyhue@hotmail.com>
2017-11-01 15:57:09 +0800

    Added demo mode as an argument while running file

--- detector.py
+++ detector.py
@@ -6,6 +6,8 @@ import roslib
 import sys
 import rospy
 import cv2
+import time
+import argparse
 
 import color
 import numpy as np
@@ -17,7 +19,7 @@ from matplotlib import pyplot as plt
 
 
 class image_converter:
-    def __init__(self):
+    def __init__(self, demo):
         image_topic = "/cameras/left_hand_camera/image"
         self.image_pub = rospy.Publisher("/robot/xdisplay", Image, latch=True, queue_size=1)
         self.bridge = CvBridge()
@@ -27,12 +29,19 @@ class image_converter:
         self.distance = {}
         self.msg = np.array([])
         self.lastImage = np.array([])
-        self.active = True
+        self.demo = demo
+        self.lastCall = 0
+        self.sampleRate = 0
 
     def display_image(self):
+        """
+            Displays image on baxter head and in window on computer
+        """
         print('Publishing to display')
         try:
             cv2.imshow("images", self.msg)
+            cv2.imwrite("finalLoc.png", self.msg)
+            cv2.waitKey(0)
             msg = self.bridge.cv2_to_imgmsg(cv2.resize(self.msg, (1024, 600), interpolation=cv2.INTER_CUBIC), "bgr8")
             self.image_pub.publish(msg)
             rospy.sleep(1)
@@ -47,15 +56,19 @@ class image_converter:
         """
         try:
             self.lastImage = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            cv2.imshow("images", self.lastImage)
+            cv2.imwrite("start.png", self.lastImage)
+            cv2.waitKey(1)
         except CvBridgeError as e:
             print(e)
-        if self.msg.shape[0] > 0 and self.active:
+        if self.msg.shape[0] > 0:
             self.msg = self.lastImage
             self.display_image()
-
-    def sensorCallback(self, msg, side):
-        self.distance[side] = msg.range
-        print("Sensor distance", side, msg.range)
+        if self.demo and time.time() - self.lastCall > self.sampleRate:
+            print("Getting domino..")
+            self.lastCall = time.time()
+            self.getNextDomino()
+            self.sampleRate = time.time() - self.lastCall
 
     def getNextDomino(self):
         """
@@ -72,22 +85,24 @@ class image_converter:
                     self.position = (nextDomino[0], nextDomino[1], nextDomino[2])
                 self.display_image()
                 self.msg = np.array([])
-                print("Domino found at")
-                print( self.position)
+                print("Domino found at", self.position)
             else:
                 print("No dominoes found")
 
         return self.position
 
-def main(args):
-    #setup_cameras()
-    #rospy.init_node('image_listener')
-    ic = image_converter()
+def main():
+    parser = argparse.ArgumentParser(description="Identifies dominoes and relative position to origin")
+    demo = False
+    parser.add_argument('-d', nargs='?', const=True, default=demo, help="Run Demo mode")
+    args = parser.parse_args()
+    ic = image_converter(args.d)
     print("Initialising...")
-    rospy.init_node('image_converter', anonymous=True)
+    rospy.init_node('image_converter', anonymous=True\
     try:
         rospy.spin()
         for i in range(10):
+            cv2.waitKey(0)
             print("Displaying...")
             ic.getNextDomino()
     except KeyboardInterrupt:
